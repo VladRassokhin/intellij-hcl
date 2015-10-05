862b13ad6ac7c289823aad5e7798630c2cfa004e
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2015-10-05 22:09:10 +0300

    Add initial version of block property completion insert handler

--- src/org/intellij/plugins/hcl/terraform/config/codeinsight/ResourcePropertyInsertHandler.kt
+++ src/org/intellij/plugins/hcl/terraform/config/codeinsight/ResourcePropertyInsertHandler.kt
@@ -18,12 +18,48 @@ package org.intellij.plugins.hcl.terraform.config.codeinsight
 import com.intellij.codeInsight.completion.BasicInsertHandler
 import com.intellij.codeInsight.completion.InsertionContext
 import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.openapi.editor.EditorModificationUtil
+import com.intellij.psi.PsiDocumentManager
+import org.intellij.plugins.hcl.terraform.config.model.PropertyOrBlockType
+import org.intellij.plugins.hcl.terraform.config.model.Type
+import org.intellij.plugins.hcl.terraform.config.model.Types
 
 object ResourcePropertyInsertHandler : BasicInsertHandler<LookupElement>() {
   override fun handleInsert(context: InsertionContext?, item: LookupElement?) {
-    // TODO: Implement
-    // Add property name
+    if (context == null || item == null) return
+    val editor = context.editor
+    val project = context.project
+    // Add property name - done by default
+
+    // TODO: Check if right part already exists
+
     // Add equals sign
+    EditorModificationUtil.insertStringAtCaret(editor, " = ")
+
     // Add value placeholder: "" for string; 0 for int, "${}" for string with IL, etc
+    val obj = item.`object`
+    if (obj is PropertyOrBlockType) {
+      val property = obj.property
+      if (property != null) {
+        val type = property.type
+        val pair = getPlaceholderValue(type)
+        if (pair != null) {
+          EditorModificationUtil.insertStringAtCaret(editor, pair.first)
+          editor.caretModel.moveToOffset(editor.caretModel.offset - pair.second)
+        }
+      }
+    }
+
+    PsiDocumentManager.getInstance(project).commitDocument(editor.document)
+  }
+
+  private fun getPlaceholderValue(type: Type): Pair<String, Int>? {
+    return when (type) {
+      Types.StringWithInjection -> Pair("\"\${}\"", 2)
+      Types.String -> Pair("\"\"", 1)
+      Types.Object -> Pair("{}", 1)
+      Types.Array -> Pair("[]", 1)
+      else -> null
+    }
   }
 }
