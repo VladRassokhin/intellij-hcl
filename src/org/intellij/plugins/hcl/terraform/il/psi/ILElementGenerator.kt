6d3a2eb4c299cf4facd130e55852b674dfd63cbf
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2015-10-24 21:54:12 +0300

    Add missing ILElementGenerator.kt

---
+++ src/org/intellij/plugins/hcl/terraform/il/psi/ILElementGenerator.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.intellij.plugins.hcl.terraform.il.psi
+
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiFileFactory
+import org.intellij.plugins.hcl.terraform.il.ILFileType
+
+public open class ILElementGenerator(val project: Project) {
+  fun createILVariable(text: String): ILVariable {
+    val file = createDummyFile(text)
+    val firstChild = file.firstChild
+    if (firstChild is ILExpressionHolder) {
+      return firstChild.ilExpression as ILVariable
+    }
+    return firstChild as ILVariable
+  }
+
+  public open fun createDummyFile(content: String): PsiFile {
+    val psiFileFactory = PsiFileFactory.getInstance(project)
+    return psiFileFactory.createFileFromText("dummy." + ILFileType.defaultExtension, ILFileType, content)
+  }
+}
