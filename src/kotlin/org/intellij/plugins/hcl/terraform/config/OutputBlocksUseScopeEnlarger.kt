1a6e8df7ac454996c9ba7b19f91ee65fec541514
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2016-02-23 06:52:39 +0300

    Search 'output' blocks usage in all project

---
+++ src/kotlin/org/intellij/plugins/hcl/terraform/config/OutputBlocksUseScopeEnlarger.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2000-2016 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.intellij.plugins.hcl.terraform.config
+
+import com.intellij.psi.PsiElement
+import com.intellij.psi.search.ProjectScopeBuilder
+import com.intellij.psi.search.SearchScope
+import com.intellij.psi.search.UseScopeEnlarger
+import getNameElementUnquoted
+import org.intellij.plugins.hcl.psi.HCLBlock
+import org.intellij.plugins.hcl.psi.HCLElement
+import org.intellij.plugins.hcl.psi.HCLIdentifier
+import org.intellij.plugins.hcl.psi.HCLStringLiteral
+
+class OutputBlocksUseScopeEnlarger : UseScopeEnlarger() {
+  override fun getAdditionalUseScope(element: PsiElement): SearchScope? {
+    if (element !is HCLElement) return null
+    if (element.containingFile.fileType != TerraformFileType) return null
+    val block: HCLBlock
+    if (element is HCLIdentifier || element is HCLStringLiteral) {
+      val parent = element.parent
+      if (parent !is HCLBlock || !parent.nameElements.contains(element)) return null
+      block = parent
+    } else if (element is HCLBlock) {
+      block = element
+    } else return null
+    if (block.getNameElementUnquoted(0) != "output") return null
+
+    val module = com.intellij.openapi.module.ModuleUtilCore.findModuleForPsiElement(element)
+    if (module != null) {
+      return module.moduleWithDependentsScope
+    }
+    return ProjectScopeBuilder.getInstance(element.project).buildProjectScope()
+  }
+}
