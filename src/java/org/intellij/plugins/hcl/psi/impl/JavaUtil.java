29c30b3d1d4dbb0da0a05c1140df1711f24c961f
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2016-07-23 08:50:00 +0300

    Improve HCLStringLiteral#getTextFragmets() calculation

--- src/java/org/intellij/plugins/hcl/psi/impl/JavaUtil.java
+++ src/java/org/intellij/plugins/hcl/psi/impl/JavaUtil.java
@@ -20,6 +20,7 @@ import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.util.text.StringUtil;
 import org.intellij.plugins.hcl.psi.HCLStringLiteral;
+import org.intellij.plugins.hcl.psi.UtilKt;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -28,65 +29,97 @@ import java.util.List;
 
 public class JavaUtil {
   private static final Key<List<Pair<TextRange, String>>> STRING_FRAGMENTS = new Key<List<Pair<TextRange, String>>>("HCL string fragments");
-  public static final String ourEscapesTable = "\"\"\\\\//b\bf\fn\nr\rt\t";
+  public static final String ourEscapesTable = "\"\"\\\\//b\bf\fn\nr\rt\tv\013a\007";
 
   @NotNull
   public static List<Pair<TextRange, String>> getTextFragments(@NotNull HCLStringLiteral literal) {
     List<Pair<TextRange, String>> result = literal.getUserData(STRING_FRAGMENTS);
-    if (result == null) {
-      result = new ArrayList<Pair<TextRange, String>>();
-      final String text = literal.getText();
-      final int length = text.length();
-      int pos = 1, unescapedSequenceStart = 1;
-      while (pos < length) {
-        if (text.charAt(pos) == '\\') {
-          if (unescapedSequenceStart != pos) {
-            result.add(Pair.create(new TextRange(unescapedSequenceStart, pos), text.substring(unescapedSequenceStart, pos)));
+    if (result != null) return result;
+    result = doGetTextFragments(literal.getText(), UtilKt.isInHCLFileWithInterpolations(literal));
+
+    literal.putUserData(STRING_FRAGMENTS, result);
+    return result;
+  }
+
+  @NotNull
+  static List<Pair<TextRange, String>> doGetTextFragments(@NotNull String text, boolean interpolations) {
+    List<Pair<TextRange, String>> result;
+    result = new ArrayList<Pair<TextRange, String>>();
+    final int length = text.length();
+    int pos = 1, unescapedSequenceStart = 1;
+    int braces = 0;
+    while (pos < length) {
+
+      final char c = text.charAt(pos);
+      if (interpolations && c == '$' && pos + 1 < length && text.charAt(pos + 1) == '{') {
+        if (unescapedSequenceStart != pos) {
+          result.add(Pair.create(new TextRange(unescapedSequenceStart, pos), text.substring(unescapedSequenceStart, pos)));
+        }
+        unescapedSequenceStart = pos;
+        pos += 2;
+        braces++;
+        while (pos < length && braces > 0) {
+          final char c2 = text.charAt(pos);
+          if (c2 == '{') {
+            braces++;
+          } else if (c2 == '}') {
+            braces--;
           }
-          if (pos == length - 1) {
-            result.add(Pair.create(new TextRange(pos, pos + 1), "\\"));
-            break;
-          }
-          final char next = text.charAt(pos + 1);
-          switch (next) {
-            case '"':
-            case '\\':
-            case '/':
-            case 'b':
-            case 'f':
-            case 'n':
-            case 'r':
-            case 't':
-              final int idx = ourEscapesTable.indexOf(next);
-              result.add(Pair.create(new TextRange(pos, pos + 2), ourEscapesTable.substring(idx + 1, idx + 2)));
-              pos += 2;
-              break;
-            case 'u':
-              int i = pos + 2;
-              for (; i < pos + 6; i++) {
-                if (i == length || !StringUtil.isHexDigit(text.charAt(i))) {
-                  break;
-                }
-              }
-              result.add(Pair.create(new TextRange(pos, i), text.substring(pos, i)));
-              pos = i;
-              break;
-            default:
-              result.add(Pair.create(new TextRange(pos, pos + 2), text.substring(pos, pos + 2)));
-              pos += 2;
-          }
-          unescapedSequenceStart = pos;
-        } else {
           pos++;
         }
+        result.add(Pair.create(new TextRange(unescapedSequenceStart, pos), text.substring(unescapedSequenceStart, pos)));
+        unescapedSequenceStart = pos;
+        continue;
       }
-      final int contentEnd = text.charAt(0) == text.charAt(length - 1) ? length - 1 : length;
-      if (unescapedSequenceStart < contentEnd) {
-        result.add(Pair.create(new TextRange(unescapedSequenceStart, length), text.substring(unescapedSequenceStart, contentEnd)));
+
+      if (c == '\\') {
+        if (unescapedSequenceStart != pos) {
+          result.add(Pair.create(new TextRange(unescapedSequenceStart, pos), text.substring(unescapedSequenceStart, pos)));
+        }
+        if (pos == length - 1) {
+          result.add(Pair.create(new TextRange(pos, pos + 1), "\\"));
+          break;
+        }
+        final char next = text.charAt(pos + 1);
+        switch (next) {
+          case '"':
+          case '\\':
+          case '/':
+          case 'a':
+          case 'b':
+          case 'f':
+          case 'n':
+          case 'v':
+          case 'r':
+          case 't':
+            final int idx = ourEscapesTable.indexOf(next);
+            result.add(Pair.create(new TextRange(pos, pos + 2), ourEscapesTable.substring(idx + 1, idx + 2)));
+            pos += 2;
+            break;
+          case 'u':
+            int i = pos + 2;
+            for (; i < pos + 6; i++) {
+              if (i == length || !StringUtil.isHexDigit(text.charAt(i))) {
+                break;
+              }
+            }
+            result.add(Pair.create(new TextRange(pos, i), text.substring(pos, i)));
+            pos = i;
+            break;
+          default:
+            result.add(Pair.create(new TextRange(pos, pos + 2), text.substring(pos, pos + 2)));
+            pos += 2;
+        }
+        unescapedSequenceStart = pos;
+      } else {
+        pos++;
       }
-      result = Collections.unmodifiableList(result);
-      literal.putUserData(STRING_FRAGMENTS, result);
     }
+    final int contentEnd = text.charAt(0) == text.charAt(length - 1) ? length - 1 : length;
+    if (unescapedSequenceStart < contentEnd) {
+      result.add(Pair.create(new TextRange(unescapedSequenceStart, contentEnd), text.substring(unescapedSequenceStart, contentEnd)));
+    }
+    result = Collections.unmodifiableList(result);
     return result;
   }
 
