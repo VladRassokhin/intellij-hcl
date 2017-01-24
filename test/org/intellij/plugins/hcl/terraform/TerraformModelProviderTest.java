51a086a7165694ee3435b97cb4381875d918d72b
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2017-01-25 02:00:02 +0300

    Workaround for: CloudFront forwarded_values should have cookies as required.
    Fixes #67

--- test/org/intellij/plugins/hcl/terraform/TerraformModelProviderTest.java
+++ test/org/intellij/plugins/hcl/terraform/TerraformModelProviderTest.java
@@ -22,6 +22,7 @@ import org.intellij.plugins.hcl.terraform.config.model.*;
 public class TerraformModelProviderTest extends LightPlatformTestCase {
   public void testModelIsLoaded() throws Exception {
     final TypeModelProvider provider = ServiceManager.getService(TypeModelProvider.class);
+    //noinspection unused
     final TypeModel model = provider.get();
   }
 
@@ -43,6 +44,34 @@ public class TerraformModelProviderTest extends LightPlatformTestCase {
     assertNotNull(access_configBlock);
     assertNotNull(findProperty(access_configBlock.getProperties(), "assigned_nat_ip"));
     assertNotNull(findProperty(access_configBlock.getProperties(), "nat_ip"));
+  }
+
+  // Test for #67
+  public void test_aws_cloudfront_distribution_forwarded_values() throws Exception {
+    final TypeModelProvider provider = ServiceManager.getService(TypeModelProvider.class);
+    assertNotNull(provider);
+    final TypeModel model = provider.get();
+    assertNotNull(model);
+
+    final ResourceType aws_cloudfront_distribution = model.getResourceType("aws_cloudfront_distribution");
+    assertNotNull(aws_cloudfront_distribution);
+    final PropertyOrBlockType[] properties = aws_cloudfront_distribution.getProperties();
+
+    final PropertyOrBlockType default_cache_behavior = findProperty(properties, "default_cache_behavior");
+    assertNotNull(default_cache_behavior);
+    final BlockType default_cache_behavior_block = default_cache_behavior.getBlock();
+    assertNotNull(default_cache_behavior_block);
+
+    final PropertyOrBlockType forwarded_values = findProperty(default_cache_behavior_block.getProperties(), "forwarded_values");
+    assertNotNull(forwarded_values);
+    final BlockType forwarded_values_block = forwarded_values.getBlock();
+    assertNotNull(forwarded_values_block);
+
+    assertNotNull(findProperty(forwarded_values_block.getProperties(), "query_string"));
+
+    PropertyOrBlockType cookies = findProperty(forwarded_values_block.getProperties(), "cookies");
+    assertNotNull(cookies);
+    assertTrue(cookies.getRequired());
   }
 
   private PropertyOrBlockType findProperty(PropertyOrBlockType[] properties, String name) {
