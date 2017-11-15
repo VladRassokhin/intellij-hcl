6fb815c5d1c8a328e60b056cafd0f47d4d8c276e
Paulo Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2017-11-15 16:38:28 +1300

    Add comment character to be chosen based on developers preferences

--- src/kotlin/org/intellij/plugins/hcl/editor/HCLCommenter.kt
+++ src/kotlin/org/intellij/plugins/hcl/editor/HCLCommenter.kt
@@ -16,11 +16,25 @@
 package org.intellij.plugins.hcl.editor
 
 import com.intellij.lang.Commenter
+import com.intellij.psi.codeStyle.CodeStyleSettingsManager
+import org.intellij.plugins.hcl.formatter.HCLCodeStyleSettings
+import kotlin.properties.Delegates
 
 
 class HCLCommenter : Commenter {
+
+  private var customSettings : HCLCodeStyleSettings by Delegates.notNull()
+
+  init {
+    customSettings = CodeStyleSettingsManager.getInstance().
+        currentSettings.getCustomSettings(HCLCodeStyleSettings::class.java)
+  }
+
   override fun getLineCommentPrefix(): String? {
-    return "//"
+    return when (HCLCodeStyleSettings.LineCommenterCharacter.values()[customSettings.PROPERTY_LINE_COMMENTER_CHARACTER]){
+      HCLCodeStyleSettings.LineCommenterCharacter.LINE_DOUBLE_SLASHES -> "//"
+      HCLCodeStyleSettings.LineCommenterCharacter.LINE_POUND_SIGN -> "#"
+    }
   }
 
   override fun getBlockCommentPrefix(): String? {
