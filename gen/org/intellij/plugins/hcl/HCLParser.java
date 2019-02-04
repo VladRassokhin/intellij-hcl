d2ebabad0e6d63fa995cd124fd5ea170866e33ef
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2019-02-04 18:26:50 +0300

    Disable inspections in generated parsers

--- gen/org/intellij/plugins/hcl/HCLParser.java
+++ gen/org/intellij/plugins/hcl/HCLParser.java
@@ -11,7 +11,7 @@ import com.intellij.psi.tree.TokenSet;
 import com.intellij.lang.PsiParser;
 import com.intellij.lang.LightPsiParser;
 
-@SuppressWarnings({"SimplifiableIfStatement", "UnusedAssignment"})
+@SuppressWarnings("ALL")
 public class HCLParser implements PsiParser, LightPsiParser {
 
   public ASTNode parse(IElementType t, PsiBuilder b) {
